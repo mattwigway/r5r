73473240771273bd09fdc33867f9755621ac2024
Matthew Wigginton Conway <matt@indicatrix.org>
2021-01-22 11:35:30 -0500

    install r5r from local source

--- install.R
+++
@@ -1,9 +0,0 @@
-install.packages('r5r', repos='https://cloud.r-project.org/')
-
-library(r5r)
-
-# prebuild graph so that binder runs are quick
-# points to directory with data
-data_path <- system.file("extdata/poa", package = "r5r")
-
-r5r_core <- setup_r5(data_path, verbose = FALSE)
